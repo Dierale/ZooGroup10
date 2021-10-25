e1f2868feae3c2af962f6e751ac0175c49096e1c
Dierale <89657232+Dierale@users.noreply.github.com>
2021-10-25 13:48:43 -0500

    Added Spring Depdendencies
    Created Spring Project with Spring Initializr and merged files into current project.  Altered pom.xml to match the one in spring project, replaced .gitignore with spring project variant.

---
+++ ZooGroup10/src/main/java/edu/nwmissouri/zoo10group/ZooGroup10Application.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.zoo10group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooGroup10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooGroup10Application.class, args);
+	}
+
+}
---
+++ ZooGroup10/src/test/java/edu/nwmissouri/zoo10group/ZooGroup10ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.zoo10group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooGroup10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
