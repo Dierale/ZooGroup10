23efdf831c55a87f72e29e7b4bce9acd575bd31b
Dierale <89657232+Dierale@users.noreply.github.com>
2021-11-04 00:06:52 -0500

    Updated IndexController
    Added some functions to generate animal lists into index controller. Added function to call animal run dynamically. Added a few dependencies and removed a path.  Still needs lots of iteration before finished

--- src/main/java/edu/nwmissouri/zoo10group/ZooIndexController.java
+++ src/main/java/edu/nwmissouri/zoo10group/ZooIndexController.java
@@ -2,7 +2,12 @@ package edu.nwmissouri.zoo10group;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Map;
+import java.util.TreeMap;
+import java.lang.reflect.Method;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -40,6 +45,8 @@ public class ZooIndexController {
         model.addAttribute("id", idParam);
         model.addAttribute("name", "World");
         model.addAttribute("greeting", GREETING_MESSAGE);
+        model.addAttribute("animalMap", getAllAnimalMap());
+        model.addAttribute("customAnimalGroup", getCustomAnimalGroup(idParam));
         // associated with index.hmtl in src/main/resources/templates
         return "index";
     }
@@ -53,16 +60,6 @@ public class ZooIndexController {
     public String about() {
         return "about";
     }
-    
-    /**
-     * Map GET request to "/user" to user().
-     *
-     * @return filename from src/main/resources/templates folder
-     */
-    @GetMapping(path = "/user")
-    public String user() {
-        return "user";
-    }
 
     /**
      * Map GET request to "/employee" to user().
@@ -73,7 +70,7 @@ public class ZooIndexController {
     public String employee() {
         return "employee";
     }
-    
+
     /**
      * Map GET request to "/visitor" to user().
      *
@@ -83,5 +80,71 @@ public class ZooIndexController {
     public String visitor() {
         return "visitor";
     }
-    
+
+    /**
+     * 76 Map GET request to "/zoo" to user().
+     *
+     * @param empTypeParam - the type of employee provided in the URL
+     * @param model - the model that holds info from controller to view
+     * @return filename from src/main/resources/templates folder
+     */
+    @GetMapping(path = "/zoo")
+    public String zoo(
+            @RequestParam(name = "type", required = false, defaultValue = "visitor") String empTypeParam, Model model) {
+        model.addAttribute("type", empTypeParam);
+        // filename for return path
+        return "zoo";
+    }
+
+    private static void callAnimalRun(String className, String animalName) {
+        String myPackage = "edu.nwmissouri.zoo10group";
+        Class[] cArg = new Class[1];
+        cArg[0] = String.class;
+        Class noparams[] = {};
+
+        String fullClass = myPackage + "." + className.replace(" ", "");
+        try {
+            Class<?> animalClass = Class.forName(fullClass);
+            Object obj = animalClass.getDeclaredConstructor(cArg).newInstance(animalName);
+            Method method = animalClass.getDeclaredMethod("run", noparams);
+            method.invoke(obj, null);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private String getCustomAnimalGroup(String id) {
+        // Create a stream to hold the output
+        var newStream = new ByteArrayOutputStream();
+        var newPrintStream = new PrintStream(newStream);
+
+        // IMPORTANT: Save the old System.out!
+        PrintStream old = System.out;
+        // Tell Java to use your special stream
+        System.setOut(newPrintStream);
+
+        var intID = Integer.parseInt(id);
+        var myList = Animal.getAnimalList();
+
+        if ((intID > 0) && (intID <= myList.length)) {
+            callAnimalRun(myList[intID - 1], myList[intID - 1] + " Tester");
+        }
+
+        // Put things back
+        System.out.flush();
+        System.setOut(old);
+        String stringOutput = newStream.toString();
+        return stringOutput;
+    }
+
+    public static Map<Integer, String> getAllAnimalMap() {
+        String[] animals = Animal.getAnimalList();
+        int animalCount = animals.length;
+        Map animalMap = Collections.synchronizedMap(new TreeMap<Integer, String>());
+
+        for (int n = 0; n < animalCount; n++) {
+            animalMap.put((n + 1), animals[n]);
+        }
+        return animalMap;
+    }
 }
