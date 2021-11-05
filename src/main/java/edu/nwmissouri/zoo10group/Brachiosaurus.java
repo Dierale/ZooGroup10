742344e3e241e464b99ee9dc2fbf44b616332161
jayanthnaramsetty <naramsetty.jc@gmail.com>
2021-11-05 12:47:06 -0500

    Fixed the Brachiosaurus class and also declared the location of the Brachiosaurus in the Brachiosaurus file

--- src/main/java/edu/nwmissouri/zoo10group/Brachiosaurus.java
+++ src/main/java/edu/nwmissouri/zoo10group/Brachiosaurus.java
@@ -6,39 +6,63 @@ package edu.nwmissouri.zoo10group;
  * @author Jayanth Naramsetty
  */
 public class Brachiosaurus extends Animal implements Runnable {
-
+/**
+ * It initialize the location of the animal
+ */
+    
+   private final String CurrentLocation;  
     /**
      * Brachiosaurus declaration
+     * 
+     * The CurrentLocation of the animal
      *
      * @param name
      */
-    public Brachiosaurus(String name) {
+   
+     public Brachiosaurus(String name) {
         super(name);
+       this.CurrentLocation= "extinct";
+        
     }
 
     /**
-     * speak() method
+     * Returns a string with what the animal says
+     * @return  The animal's speech as a string
      */
-    public void speak() {
-        System.out.println("I can Roar,Grunts,Hisses,Squeaks");
+    public String speak() {
+        return "I can Roar,Grunts,Hisses,Squeaks";
     }
 
     /**
-     * move() method
+     * Returns a String with the moment of the animal
+     * @return The animal moment as a string
      */
-    public void move() {
-        System.out.println("i walk all around the land ");
+    public String move() {
+        return "i walk all around the land ";
+        
     }
 
     /**
-     * facts() methods
+     * Returns a String of the facts which contains info about the animal
+     * @return The animal facts a String
      */
-    public void facts() {
-        System.out.println("WE are group of reptiles that have lived on Earth\nfor about 245 million years. ");
+    public String facts() {
+        return "WE are group of reptiles that have lived on Earth"
+                + "for about 245 million years.";
+        
     }
 
+    public String getCurrentLocation() {
+        return this.CurrentLocation;
+    }
+    
     @Override
     public void run() {
+        System.out.println("Hai Humans my name is "+ getName());
+        System.out.println(speak());
+        System.out.println(move());
+        System.out.println(facts());
+        System.out.println("The Currentlocation of the Brachiosaurus is: " + getCurrentLocation());
 
     }
 
